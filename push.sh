03da5f3bf033fef494886e1754013ac94bd96272
root <root@localhost.localdomain>
2023-03-14 11:06:35 +0800

    submit something

--- push.sh
+++ push.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+#
+
+
 git add .
 git commit -m "submit something"
-git push -u origin master
+
+/usr/bin/expect << eof
+set timeout 3
+spawn git push -u origin master
+expect "//gitee.com':"
+send "cokeice\n"
+expect "@gitee.com':"
+send "3w.CNGNWLF.com\n
+expect eof
+eof
+
+
+
